b68459a0436beca7baad9368f886b9911ab259bb
Luis E. González <luisegr149@gmail.com>
2020-03-06 12:31:05 -0600

    test

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const soap = require('soap');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var myService = {
+    MyService: {
+        MyPort: {
+            MyFunction: function (args) {
+                console.log('args :', args);
+                return {
+                    name: args.name
+                };
+            },
+        }
+    }
+};
+
+var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
+
+//http server example
+// var server = http.createServer(function (request, response) {
+//     response.end('404: Not Found: ' + request.url);
+// });
+
+// server.listen(8000);
+// soap.listen(server, '/wsdl', myService, xml);
+
+//express server example
+var app = express();
+//body parser middleware are supported (optional)
+app.use(bodyParser.raw({ type: function () { return true; }, limit: '5mb' }));
+
+app.use('*', (req, res, next) => {
+    console.log("\n\n\nNewRequest-----", (new Date()).toISOString());
+    console.log("RequestHeaders:\n", req.headers);
+    // console.log("RequestBody:\n", req.body);
+    let body = Buffer.from(req.body).toString('utf-8');
+    console.log('body :\n', body);
+    next();
+})
+
+app.listen(8001, function () {
+    //Note: /wsdl route will be handled by soap module
+    //and all other routes & middleware will continue to work
+    soap.listen(app, '/wsdl', myService, xml);
+});
